3e90ca380c0aabe0893ee300f41f27dc35351628
Mohammadsahilshabbir <sahilshabbir4568@gmail.com>
2026-01-18 23:30:07 +0530

    Time: 7 ms (94.41%) | Memory: 47 MB (55.78%) - LeetSync

--- 1260-day-of-the-year/day-of-the-year.java
+++ 1260-day-of-the-year/day-of-the-year.java
@@ -1,4 +1,10 @@
 class Solution {
+    static boolean isLeap(int year) {
+        if(year%400==0) return true;
+        if(year%100==0) return false;
+        return (year%4==0) ;
+        
+    }
     public int dayOfYear(String date) {
 
         int year = Integer.parseInt(date.substring(0, 4));
@@ -7,15 +13,12 @@ class Solution {
 
         int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-       
-        boolean leap = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
-
         int totalDays = day;
 
         for (int i = 0; i < month - 1; i++) {
             totalDays += days[i];
         }
-
+        boolean leap=isLeap(year);
         
         if (leap && month > 2) {
             totalDays++;
