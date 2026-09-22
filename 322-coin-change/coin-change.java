da09686c2d6423e6b774e2637a00da38163f800c
Mohammadsahilshabbir <sahilshabbir4568@gmail.com>
2026-09-22 09:51:06 +0530

    Time: 35 ms (13.26%) | Memory: 48.4 MB (5.20%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,24 +3,20 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         Integer[][] dp=new Integer[coins.length+1][amount+1];
         int ans = helper(coins, amount, coins.length - 1, dp);
-
-        if(ans == Integer.MAX_VALUE)
+        if(ans >= 10000000)
             return -1;
-
         return ans;
 
     }
     public int helper(int[] coins,int amount,int i,Integer[][] dp){
         if(amount==0) return 0;
-        if(i<0) return Integer.MAX_VALUE;
+        if(i<0) return 100000000;
         if(dp[i][amount]!=null) return dp[i][amount];
-        int take = Integer.MAX_VALUE;
+        int take = 100000000;
         if(coins[i] <= amount) {
 
-            int result = helper(coins, amount - coins[i], i, dp);
-
-            if(result != Integer.MAX_VALUE)
-                take = 1 + result;
+            take =1+ helper(coins, amount - coins[i], i, dp);
+            
         }
         int notTake=helper(coins,amount,i-1,dp);
         return dp[i][amount]=Math.min(take,notTake);
